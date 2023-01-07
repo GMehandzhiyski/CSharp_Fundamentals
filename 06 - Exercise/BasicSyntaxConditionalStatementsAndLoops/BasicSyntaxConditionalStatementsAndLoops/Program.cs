73c849761362f2807777b4cfac47e909c505bc85
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2023-01-07 17:21:40 +0200

    Age 100% DONE

---
+++ 06 - Exercise/BasicSyntaxConditionalStatementsAndLoops/BasicSyntaxConditionalStatementsAndLoops/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicSyntaxConditionalStatementsAndLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
