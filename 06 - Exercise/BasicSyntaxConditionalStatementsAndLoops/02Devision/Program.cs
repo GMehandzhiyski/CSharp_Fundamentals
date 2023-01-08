a91b7c2aa929ad1f519567356b052c5d64fb1e45
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2023-01-08 21:07:03 +0200

    02Devision 100% Done

--- 06 - Exercise/BasicSyntaxConditionalStatementsAndLoops/02Devision/Program.cs
+++ 06 - Exercise/BasicSyntaxConditionalStatementsAndLoops/02Devision/Program.cs
@@ -6,7 +6,35 @@ namespace _02Devision
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = int.Parse(Console.ReadLine());
+            int magicNumber = 0;
+
+            if (number % 10 == 0)
+            {
+                magicNumber = 10;
+            }
+            else if (number % 7 == 0)
+            {
+                magicNumber = 7;
+            }
+            else if (number % 6 == 0)
+            {
+                magicNumber = 6;
+            }
+            else if (number % 3 == 0)
+            {
+                magicNumber = 3;
+            }
+            else if (number % 2 == 0)
+            {
+                magicNumber = 2;
+            }
+            else
+            {
+                Console.WriteLine($"Not divisible");
+                return;
+            }
+                Console.WriteLine($"The number is divisible by {magicNumber}.");
         }
     }
 }
