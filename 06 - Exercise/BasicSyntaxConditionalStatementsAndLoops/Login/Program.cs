b80c0ec6746d846dd6626d4c77ae523cf54b6c04
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2023-01-10 22:08:37 +0200

    Login 100% Done

---
+++ 06 - Exercise/BasicSyntaxConditionalStatementsAndLoops/Login/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            int counterPassword = 0;
+            string passwordMirror = string.Empty;
+            string passwordLast = string.Empty;
+
+            for (int i = password.Length - 1; i >= 0; i--)
+            {
+                passwordMirror += password[i];
+            }
+            
+            while ((passwordMirror != (passwordLast = Console.ReadLine())) && counterPassword < 3)
+            {
+                Console.WriteLine("Incorrect password. Try again.");
+                counterPassword++;
+            }
+
+            if (passwordLast == passwordMirror)
+            {
+                Console.WriteLine($"User {password} logged in.");
+            }
+            else 
+            {
+                Console.WriteLine($"User {password} blocked!");
+            }
+            
+        }
+    }
+}
