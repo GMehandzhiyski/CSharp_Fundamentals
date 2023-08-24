e3c0c5a57bc050359516f9c3012ae16518ba9189
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-08-24 10:35:15 +0300

    01.Ages DONE

--- 06 - Basic Syntax, Conditional Statements and Loops/DataTypesAndVariables - Exercise/01.IntegerOperations/Program.cs
+++ 06 - Basic Syntax, Conditional Statements and Loops/DataTypesAndVariables - Exercise/01.IntegerOperations/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace _01.IntegerOperations
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace _01.IntegerOperations
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, ld!");
+            int age = int.Parse(Console.ReadLine());
+            string person = string.Empty;
+
+            if (age >= 0 && age <= 2)
+            {
+                person = "baby";
+            }
+            else if (age >= 3 && age <= 13)
+            {
+                person = "child";
+            }
+            else if (age >= 14 && age <= 19)
+            {
+                person = "teenager";
+            }
+            else if (age >= 20 && age <= 65)
+            {
+                person = "adult";
+            }
+            else if (age >= 66 )
+            {
+                person = "elder";
+            }
+
+            Console.WriteLine($"{person}");
         }
+        
     }
 }
---
+++ 06 - Basic Syntax, Conditional Statements and Loops/DataTypesAndVariables - Exercise/02.Devision/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _02.Devision
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int numberDiv;
+
+            if (number % 10 == 0) 
+            {
+                numberDiv = 10;
+            }
+            else if (number % 7 == 0) 
+            {
+                numberDiv = 7;
+            }
+            else if(number % 6 == 0) 
+            {
+                numberDiv = 6;
+            }
+            else if (number % 3 == 0)
+            {
+                numberDiv = 3;
+            }
+            else if (number % 2 == 0)
+            {
+                numberDiv = 2;
+            }
+        }
+    }
+}
