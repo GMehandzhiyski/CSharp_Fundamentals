16664c981ec4ebe83b687d974e573ac0fcf73ff4
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-08-24 23:06:01 +0300

    06.StrongNumber 100%DOne

---
+++ 06 - Basic Syntax, Conditional Statements and Loops/DataTypesAndVariables - Exercise/06.StrongNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _06.StrongNumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+            int originalNum = number;   
+            int digit;
+            int sum = 1;
+            int secondSum = 0;
+
+            while (number > 0)
+            {
+                sum = 1;
+                digit = number % 10;
+                number = number / 10;
+                //Console.WriteLine(digit);
+                
+                for (int i = 1; i <= digit; i++)
+                {
+                    sum *= i;
+                }
+
+                secondSum = sum + secondSum;
+
+            }
+
+            if (originalNum== secondSum)
+            {
+                Console.WriteLine("yes");
+            }
+            else 
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
