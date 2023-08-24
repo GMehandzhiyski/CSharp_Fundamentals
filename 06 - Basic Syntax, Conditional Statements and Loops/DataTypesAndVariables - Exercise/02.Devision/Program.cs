782b2d0d6d611354ad7adfc2b7ad087e3f0752ff
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-08-24 10:46:22 +0300

    02.Division 100%Done

--- 06 - Basic Syntax, Conditional Statements and Loops/DataTypesAndVariables - Exercise/02.Devision/Program.cs
+++ 06 - Basic Syntax, Conditional Statements and Loops/DataTypesAndVariables - Exercise/02.Devision/Program.cs
@@ -1,32 +1,41 @@
-﻿namespace _02.Devision
+﻿using System;
+
+namespace _02.Devision
 {
     public class Program
     {
         static void Main(string[] args)
         {
             int number = int.Parse(Console.ReadLine());
-            int numberDiv;
+           
 
-            if (number % 10 == 0) 
+            if (number % 10 == 0)
             {
-                numberDiv = 10;
+                Console.WriteLine($"The number is divisible by 10");
             }
-            else if (number % 7 == 0) 
+            else if (number % 7 == 0)
             {
-                numberDiv = 7;
+                Console.WriteLine($"The number is divisible by 7");
             }
-            else if(number % 6 == 0) 
+            else if (number % 6 == 0)
             {
-                numberDiv = 6;
+                Console.WriteLine($"The number is divisible by 6");
             }
             else if (number % 3 == 0)
             {
-                numberDiv = 3;
+                Console.WriteLine($"The number is divisible by 3");
             }
             else if (number % 2 == 0)
             {
-                numberDiv = 2;
+                Console.WriteLine($"The number is divisible by 2");
             }
+            else 
+            {
+                Console.WriteLine($"Not devisible");
+            }
+
+          
+
         }
     }
 }
