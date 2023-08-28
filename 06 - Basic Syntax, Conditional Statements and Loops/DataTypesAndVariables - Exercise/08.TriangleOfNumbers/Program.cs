c29cc150c4798712e2d90dcb51bd4ee392009701
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-08-28 21:43:35 +0300

    08.TriangleOfNumbers 100%done

---
+++ 06 - Basic Syntax, Conditional Statements and Loops/DataTypesAndVariables - Exercise/08.TriangleOfNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _08.TriangleOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int ii = 1; ii <= i; ii++)
+                {
+                    Console.Write(i + " "); 
+                  
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
