38c322ee88a96357ad0852d6a4fc8674c1e9dfc7
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-08-29 16:32:06 +0300

    10.RageExpensens 100%Done

--- 06 - Basic Syntax, Conditional Statements and Loops/DataTypesAndVariables - Exercise/10.RageExpenses/Program.cs
+++ 06 - Basic Syntax, Conditional Statements and Loops/DataTypesAndVariables - Exercise/10.RageExpenses/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           // Input Value
+           int lostGames = int.Parse(Console.ReadLine());
+           double priceHeadset = double.Parse(Console.ReadLine());    
+           double priceMouse = double.Parse(Console.ReadLine()); 
+           double priceKeyboard = double.Parse(Console.ReadLine());
+           double priceDisplay = double.Parse(Console.ReadLine());
+
+            //Headset Trash
+            double trashedHeadset = (lostGames / 2);
+            //Mouse Trash
+            double trashedMouse = (lostGames / 3);
+            //Keyboard Trash
+            double trashedKeyboard = (lostGames / 6);
+            //Display Trash
+            double trashedDisplay = (lostGames / 12);
+            // 
+
+            double totalHeadsetPrice = Math.Round(trashedHeadset) * priceHeadset;
+            double totalMousePrice = Math.Round(trashedMouse) * priceMouse;
+            double totalKeyboardPrice = Math.Round(trashedKeyboard) * priceKeyboard;
+            double totalDisplayPrice = Math.Round(trashedDisplay) * priceDisplay;
+
+            double generalSetPrice = totalHeadsetPrice + totalMousePrice + totalKeyboardPrice + totalDisplayPrice;
+
+
+             /*
+            Console.WriteLine(trashedHeadset);
+            Console.WriteLine(trashedMouse);
+            Console.WriteLine(trashedKeyboard);
+            Console.WriteLine(trashedDisplay);
+            */
+
+
+            //Output Value
+            Console.WriteLine($"Rage expenses: {generalSetPrice:f2} lv.");
         }
     }
 }
