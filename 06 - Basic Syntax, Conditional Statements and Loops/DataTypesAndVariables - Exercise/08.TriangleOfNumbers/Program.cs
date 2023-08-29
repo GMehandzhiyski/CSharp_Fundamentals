f4d89eb9968de33315357a8320f74f3ac45c3843
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-08-29 14:08:55 +0300

    09.PadawanEquipment 100%done

--- 06 - Basic Syntax, Conditional Statements and Loops/DataTypesAndVariables - Exercise/08.TriangleOfNumbers/Program.cs
+++ 06 - Basic Syntax, Conditional Statements and Loops/DataTypesAndVariables - Exercise/08.TriangleOfNumbers/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace _08.TriangleOfNumbers
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
--- 06 - Basic Syntax, Conditional Statements and Loops/DataTypesAndVariables - Exercise/09.PadawanEquipment/Program.cs
+++ 06 - Basic Syntax, Conditional Statements and Loops/DataTypesAndVariables - Exercise/09.PadawanEquipment/Program.cs
@@ -1,10 +1,47 @@
 ﻿namespace _09.PadawanEquipment
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double money = double.Parse(Console.ReadLine());    
+            int studnets = int.Parse(Console.ReadLine()); 
+            double priceLightsabers = double.Parse(Console.ReadLine()); 
+            double priceRobes = double.Parse(Console.ReadLine());   
+            double priceBelts = double.Parse(Console.ReadLine());
+
+            double totalPriceLightsabers = 0;
+            double totalPriceRobes = 0;
+            double totalPriceBelts = 0;
+            double totalLighsabresNumbers = 0;
+            double freeBelts = 0;
+            double generalPrice = 0;
+
+            // lighsabres
+            totalLighsabresNumbers = Math.Ceiling (studnets * 1.1) ;
+            totalPriceLightsabers = (totalLighsabresNumbers * priceLightsabers);
+            // Robes
+            totalPriceRobes = studnets * priceRobes;
+            //Belst
+            freeBelts = (studnets / 6);
+            totalPriceBelts = (studnets - freeBelts) * priceBelts;
+
+            generalPrice = totalPriceLightsabers + totalPriceRobes + totalPriceBelts;
+
+
+            if (generalPrice <= money )
+            {
+                Console.WriteLine($"The money is enough - it would cost {(generalPrice):F2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"John will need { Math.Abs(money - generalPrice):F2}lv more.");
+            }
+
+            
+
+
+
         }
     }
 }
