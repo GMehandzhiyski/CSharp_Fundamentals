84d41ee11f288a8406e3870b497b676725124ff9
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-08-29 16:54:39 +0300

    11.Order 100%Done

--- 06 - Basic Syntax, Conditional Statements and Loops/DataTypesAndVariables - Exercise/10.RageExpenses/Program.cs
+++ 06 - Basic Syntax, Conditional Statements and Loops/DataTypesAndVariables - Exercise/10.RageExpenses/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace _10.RageExpenses
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
--- 06 - Basic Syntax, Conditional Statements and Loops/DataTypesAndVariables - Exercise/11.Order/Program.cs
+++ 06 - Basic Syntax, Conditional Statements and Loops/DataTypesAndVariables - Exercise/11.Order/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace _11.Order
+﻿using System.Diagnostics;
+
+namespace _11.Order
 {
-    internal class Program
+   public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numberOfOrder =int.Parse(Console.ReadLine());
+
+            double generalSum = 0;
+            double finishgeneralSum = 0;
+
+            for (int i = 0; i < numberOfOrder; i++)
+            {
+                double capsulePrice = double.Parse(Console.ReadLine());
+                double days = double.Parse(Console.ReadLine());
+                int capsuleCount = int.Parse(Console.ReadLine());
+
+                generalSum = capsulePrice * days * capsuleCount;
+                Console.WriteLine($"The price for the coffee is: ${generalSum:f2}");
+                finishgeneralSum += generalSum;
+            }
+            
+           
+            Console.WriteLine($"Total: ${finishgeneralSum:f2}"); 
+
+
         }
     }
 }
