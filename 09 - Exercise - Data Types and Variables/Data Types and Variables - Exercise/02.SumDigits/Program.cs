18e4b4408bf1d3502e47bfafa85517fad494e740
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-09-01 16:06:54 +0300

    02.SumDigits 100%DOne

---
+++ 09 - Exercise - Data Types and Variables/Data Types and Variables - Exercise/02.SumDigits/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _02.SumDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int sum1 = 0;
+            while (number != 0) 
+            {
+                sum = number % 10;
+                sum1 += sum ;
+                number = number / 10;
+
+            }
+            Console.WriteLine(sum1);
+
+
+
+
+        }
+    }
+}
