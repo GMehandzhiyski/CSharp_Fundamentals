d7de8a6775d7332c7141b320c93cf2996ac074b4
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-09-01 16:08:04 +0300

    04.SumOfChars 100%Done

---
+++ 09 - Exercise - Data Types and Variables/Data Types and Variables - Exercise/04.SumOfChars/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _04.SumOfChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int sum1 = 0;
+
+            for (int i = 0; i < number; i++) 
+            { 
+               char charecter = char.Parse( Console.ReadLine());
+
+                sum = (int)(charecter);
+                sum1 += sum;
+            }
+
+            Console.WriteLine(sum1);
+
+        }
+    }
+}
