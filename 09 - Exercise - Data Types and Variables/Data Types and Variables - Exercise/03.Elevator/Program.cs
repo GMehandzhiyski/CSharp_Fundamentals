6fe656de8d1641a53c82421740d28e56e4dfe89c
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-09-01 16:07:20 +0300

    03.Elevator

---
+++ 09 - Exercise - Data Types and Variables/Data Types and Variables - Exercise/03.Elevator/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _03.Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberOfPeople = int.Parse(Console.ReadLine());
+            double capacityOfElevator = int.Parse(Console.ReadLine());   
+
+            double supposedCourses =Math.Ceiling( numberOfPeople / capacityOfElevator);
+            Console.WriteLine(supposedCourses);
+          
+
+        }
+    }
+}
