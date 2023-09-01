c2549378a9187d835088f26758bb787bced1abf2
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-09-01 16:29:03 +0300

    06.TripolesOfLatinsLatters

---
+++ 09 - Exercise - Data Types and Variables/Data Types and Variables - Exercise/06.TriolesOfLatinsLatters/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _06.TriolesOfLatinsLatters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());    
+
+            for (int i = 1; i < number ; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        Console.WriteLine(k);
+
+                    }
+
+                }
+            }     
+        }
+    }
+}
