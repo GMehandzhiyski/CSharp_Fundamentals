fb60f5990ff80292a7025070626916eeeee8c6a4
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-09-01 16:09:05 +0300

    Data Types and Variable crate

---
+++ 09 - Exercise - Data Types and Variables/Data Types and Variables - Exercise/Data Types and Variables - Exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Data_Types_and_Variables___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
