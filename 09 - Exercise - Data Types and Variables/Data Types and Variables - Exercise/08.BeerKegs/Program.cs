b88eb4747d333e5c3439c5b2db9f9cd575ab3aa9
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-09-05 11:08:55 +0300

    08.Beerkegs 100%Done

---
+++ 09 - Exercise - Data Types and Variables/Data Types and Variables - Exercise/08.BeerKegs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Security;
+
+namespace _08.BeerKegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            double lastVolume = 0;
+            string lastVolumeName = string.Empty;
+
+            for (int i = 1; i <= number; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+
+                double volume = Math.PI * (Math.Pow(radius, 2) * height);
+
+                if (volume >= lastVolume)
+                {
+                    lastVolume = volume;
+                    lastVolumeName = model;
+                }
+            }
+
+            Console.WriteLine(lastVolumeName);
+        }
+    }
+}
