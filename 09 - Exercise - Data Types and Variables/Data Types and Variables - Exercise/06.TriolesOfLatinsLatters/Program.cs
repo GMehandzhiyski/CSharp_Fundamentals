5981273a5f10eb1353a1bf82e099c805f1313580
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-09-05 10:20:05 +0300

    06.TriolesOfLatinsLatters 100%Done

--- 09 - Exercise - Data Types and Variables/Data Types and Variables - Exercise/06.TriolesOfLatinsLatters/Program.cs
+++ 09 - Exercise - Data Types and Variables/Data Types and Variables - Exercise/06.TriolesOfLatinsLatters/Program.cs
@@ -3,21 +3,47 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            int number = int.Parse(Console.ReadLine());    
+        { /*
+            int number = int.Parse(Console.ReadLine());
+            char offsetChar = 'a';
+            int offset = (int)(offsetChar);
 
             for (int i = 1; i < number ; i++)
             {
-                for (int j = 0; j < n; j++)
+                for (int j = 0; j < number ; j++)
                 {
-                    for (int k = 0; k < n; k++)
+
+                    for (int k = 0; k < number ; k++)
                     {
-                        Console.WriteLine(k);
+                        char line1 = (char)(i);
+                        char line2 = (char)(j);
+                        char line3 = (char)(k);
+
+
+                        char firstChar = (char)('a' + i);
 
                     }
 
+                }*/
+            int number = int.Parse(Console.ReadLine());
+
+
+            for (char i = 'a'; i < 'a' + number; i++)
+            {
+
+                for (char k = 'a'; k < 'a' + number; k++)
+                {
+
+
+                    for (char l = 'a'; l < 'a' + number; l++)
+                    {
+
+
+                        Console.WriteLine($"{i}{k}{l}");
+                    }
+
                 }
-            }     
+            }
         }
     }
 }
