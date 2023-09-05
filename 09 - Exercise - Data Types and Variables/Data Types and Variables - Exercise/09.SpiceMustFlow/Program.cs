d7be4f26515749563dc4607e224331d7e8061969
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-09-05 13:48:07 +0300

    09.SpiceMustFlow 57%Done

---
+++ 09 - Exercise - Data Types and Variables/Data Types and Variables - Exercise/09.SpiceMustFlow/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Serialization;
+
+namespace _09.SpiceMustFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int day = 0;
+            int spices = 0;
+            int spices1 = 0;
+
+
+            while (startingYield >= 100)
+            {
+                spices1 = startingYield - 26;
+                spices = spices + spices1;
+                startingYield -= 10;
+
+                day++; 
+            }
+            Console.WriteLine(day);
+            Console.WriteLine(spices - 26);  
+        }
+    }
+}
