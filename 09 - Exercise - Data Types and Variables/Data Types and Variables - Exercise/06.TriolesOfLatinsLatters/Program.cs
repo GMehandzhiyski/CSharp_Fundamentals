77072111665f1430396213f9186fc18ead6c3632
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-09-05 10:34:37 +0300

    07.WaterOverflow 100%Done

--- 09 - Exercise - Data Types and Variables/Data Types and Variables - Exercise/06.TriolesOfLatinsLatters/Program.cs
+++ 09 - Exercise - Data Types and Variables/Data Types and Variables - Exercise/06.TriolesOfLatinsLatters/Program.cs
@@ -3,28 +3,8 @@
     internal class Program
     {
         static void Main(string[] args)
-        { /*
-            int number = int.Parse(Console.ReadLine());
-            char offsetChar = 'a';
-            int offset = (int)(offsetChar);
-
-            for (int i = 1; i < number ; i++)
-            {
-                for (int j = 0; j < number ; j++)
-                {
-
-                    for (int k = 0; k < number ; k++)
-                    {
-                        char line1 = (char)(i);
-                        char line2 = (char)(j);
-                        char line3 = (char)(k);
-
-
-                        char firstChar = (char)('a' + i);
-
-                    }
-
-                }*/
+        { 
+           
             int number = int.Parse(Console.ReadLine());
 
 
---
+++ 09 - Exercise - Data Types and Variables/Data Types and Variables - Exercise/07.WaterOverflow/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _07.WaterOverflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int totalCapacity = 0;
+
+            for (int i = 1; i <= number; i++) 
+            { 
+                int addLitre = int.Parse(Console.ReadLine());
+
+                totalCapacity += addLitre;
+
+                if (totalCapacity > 255)
+                {
+                    totalCapacity -= addLitre;
+                    Console.WriteLine("Insufficient capacity!");
+                } 
+            }
+            Console.WriteLine(totalCapacity);
+        }
+    }
+}
