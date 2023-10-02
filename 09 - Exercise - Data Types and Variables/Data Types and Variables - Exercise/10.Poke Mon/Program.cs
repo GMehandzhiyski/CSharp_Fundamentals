3fde6ad262c768819ddcc541a14cc2500c5c0c0f
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-02 13:28:55 +0300

    10.Poke Mon done 90%

---
+++ 09 - Exercise - Data Types and Variables/Data Types and Variables - Exercise/10.Poke Mon/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _10.Poke_Mon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pokePower = int.Parse(Console.ReadLine());// N
+            int pokeDistance = int.Parse(Console.ReadLine()); // M  
+            int pokeExhaustionFactor = int.Parse(Console.ReadLine()); // Y   
+
+
+            int pokePoweProces = 0;
+            pokePoweProces =  pokePower; // take from N
+
+            int countTargets = 0;
+            while (pokeDistance <= pokePoweProces ) 
+            {
+                pokePoweProces = pokePoweProces - pokeDistance;
+                countTargets++;// targets
+
+                int middelPokePower = pokePower / 2; //50% of begin value
+
+                if (pokePoweProces == middelPokePower){pokePoweProces = pokePoweProces / pokeExhaustionFactor;}
+            }
+
+            Console.WriteLine(pokePoweProces);
+            Console.WriteLine(countTargets);
+
+            
+        }
+    }
+}
