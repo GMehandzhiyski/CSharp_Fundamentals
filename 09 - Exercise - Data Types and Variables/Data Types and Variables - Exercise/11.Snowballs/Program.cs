bc5cdd15533d445ecea0e21ea85ee221cd3b3db4
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-02 15:20:34 +0300

    10.Snowballs done 100%

--- 09 - Exercise - Data Types and Variables/Data Types and Variables - Exercise/11.Snowballs/Program.cs
+++ 09 - Exercise - Data Types and Variables/Data Types and Variables - Exercise/11.Snowballs/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace _11.Snowballs
+﻿using System.Numerics;
+
+namespace _11.Snowballs
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int snowBalls = int.Parse(Console.ReadLine());
+
+
+            int snowBallSnow = 0;
+            int snowBallTime = 0;
+            int snowBallQuality = 0;
+            BigInteger snowBallValue = 0;
+            BigInteger snowBallValueProces = 0;
+            int snowBallSnowBig = 0;
+            int snowBallTimeBig = 0;
+            int snowBallQualityBig = 0;
+            while (snowBalls > 0)
+            {
+               snowBallSnow = int.Parse(Console.ReadLine());
+               snowBallTime = int.Parse(Console.ReadLine());   
+               snowBallQuality = int.Parse(Console.ReadLine());
+
+                snowBallValue = BigInteger.Pow((snowBallSnow / snowBallTime), snowBallQuality);
+
+                if (snowBallValue >= snowBallValueProces)
+                {
+                    snowBallValueProces =  snowBallValue;
+                    snowBallSnowBig = snowBallSnow;
+                    snowBallTimeBig = snowBallTime;
+                    snowBallQualityBig = snowBallQuality;
+                }
+
+                snowBalls--;
+            }
+
+            Console.WriteLine($"{snowBallSnowBig} : {snowBallTimeBig} = {snowBallValueProces} ({snowBallQualityBig})");
+        
         }
     }
 }
