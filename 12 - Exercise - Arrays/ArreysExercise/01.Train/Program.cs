cba7412fb58d9513e00c07ac5b2f5656d9e0946d
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-09 21:51:42 +0300

    01.Train 100% done

---
+++ 12 - Exercise - Arrays/ArreysExercise/01.Train/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks.Dataflow;
+  
+namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int number = int.Parse(Console.ReadLine());
+            int [] wagon = new int [number];
+
+            for (int i = 0; i < number; i++)
+            {
+                wagon[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine(string.Join(" ",wagon));
+            Console.WriteLine(wagon.Sum());
+        }
+    }
+}
---
+++ 12 - Exercise - Arrays/ArreysExercise/ArreysExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ArreysExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] arr = Console.ReadLine().Split();
+
+            //string temp = arr[0];
+
+            //for (int i = 0 ; i < arr.Length - 1 ; i++) 
+            //{
+            //    arr[i] = arr[i +  1];
+
+            //}
+            //arr[arr.Length - 1] = temp;
+
+
+            //Console.WriteLine(string.Join(" ", arr));
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    // Ако i е четно, пропусни текущата итерация и продължи със следващата.
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
