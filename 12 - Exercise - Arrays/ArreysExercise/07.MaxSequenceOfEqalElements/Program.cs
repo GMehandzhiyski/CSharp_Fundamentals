8f34a9cd377569253d73f573569ba0a4e62d06ba
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-10 21:52:44 +0300

    07.MaxSequenceofEqialElelements 100%done

---
+++ 12 - Exercise - Arrays/ArreysExercise/07.MaxSequenceOfEqalElements/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _07.MaxSequenceOfEqalElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < array.Length; i++) 
+            {
+                if (array[i] == array[i + 1])
+                { 
+                
+                }
+            
+            }
+        }
+    }
+}
