f19752b42b03f338b7d192eafa40abaa22ef8890
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-10 21:14:12 +0300

    05.TopIntiger 100% Done

---
+++ 12 - Exercise - Arrays/ArreysExercise/05.TopIntegers/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _05.TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string temp = "";
+           
+            for (int i = 0; i < array.Length; i++)
+            {
+                int curr = array[i];
+                bool isTopInteger = true;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (curr <= array[j])
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+
+                }
+
+                if (isTopInteger)
+                {
+                    temp += curr + " ";
+                }
+
+            }
+
+           
+
+            Console.WriteLine(string.Join(" ", temp));    
+        }
+    }
+}
