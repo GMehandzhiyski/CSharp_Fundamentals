c4d7ad9c11a482448ea768d8eba16aa3c814b06c
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-10 10:23:19 +0300

    04.ArrayRotation 100% done

--- 12 - Exercise - Arrays/ArreysExercise/04.ArrayRotation/Program.cs
+++ 12 - Exercise - Arrays/ArreysExercise/04.ArrayRotation/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] array = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+
+          
+
+            for (int i = 1; i <= n; i++)
+            {
+                string temp = array[0];
+
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    array[j] = array[j + 1];
+                   
+                }
+
+
+                 array[array.Length - 1] = temp;
+
+            }
+
+
+
+            Console.WriteLine(string.Join(" ",array));
         }
     }
 }
