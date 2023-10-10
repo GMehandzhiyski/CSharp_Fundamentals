70631e74957df82c2e824c200eb4ae4e08360d55
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-10 21:43:15 +0300

    06.EqualSums 100% done

---
+++ 12 - Exercise - Arrays/ArreysExercise/06.EqualSums/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _06.EqualSums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            //int curr = 3;
+            bool isHaveEqual = false;
+            
+
+            for (int curr  = 0; curr < array.Length; curr++)
+            {
+                int rightResult = 0;
+                for (int i = curr + 1; i < array.Length; i++)
+                {
+                    rightResult += array[i];
+                }
+                int leftResult = 0;
+                for (int i = curr - 1; i >= 0; i--)
+                {
+                    leftResult += array[i];
+                }
+            
+                if (rightResult == leftResult) 
+                {
+
+                    Console.WriteLine(curr);
+                    isHaveEqual = true;
+                }
+
+            }
+            if (isHaveEqual == false)
+            { 
+                Console.WriteLine("no"); 
+            }
+           
+        }
+    }
+}
