b017e4f6c51aff575f76d85b8f134b5c3d832eaf
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-10 06:24:46 +0300

    03.Zig-Zag Arrays 100% done

---
+++ 12 - Exercise - Arrays/ArreysExercise/03.Zig-ZagArrays/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _03.Zig_ZagArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string[] arraysOne = new string[number];
+            string[] arraysTwo = new string[number];
+
+            for (int i = 0; i <= number - 1; i++) 
+            {
+
+                string[] current = Console.ReadLine().Split();
+
+                if (i % 2 == 0)
+                {
+                    arraysOne[i] = current[0];
+                    arraysTwo[i] = current[1];
+                }
+                else 
+                {
+                    arraysOne[i] = current[1];
+                    arraysTwo[i] = current[0];
+                }
+                
+               
+            }
+
+            Console.WriteLine(string.Join(" ", arraysOne));
+            Console.WriteLine(string.Join(" ", arraysTwo));
+
+
+        }
+    }
+}
