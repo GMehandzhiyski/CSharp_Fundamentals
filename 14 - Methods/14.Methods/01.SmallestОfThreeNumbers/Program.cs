5425856eba40f34023bf9763d8d78aea9f48b8a1
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-13 06:32:15 +0300

    01.SmallestOfThreeNumbers 100% done

---
+++ 14 - Methods/14.Methods/01.SmallestОfThreeNumbers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _01.SmallestОfThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+
+            smallestInt(number1, number2, number3);
+        }
+
+
+        // 600
+        // 342
+        // 123
+        private static void smallestInt(int number1, int number2, int number3)
+        {
+            if (number1 <= number2)
+            {
+                if (number1 <= number3)
+                {
+                    Console.WriteLine(number1);
+                }
+                else
+                {
+                    Console.WriteLine(number3);
+                }
+           
+
+            }
+            else
+            {
+                if (number2 <= number3)
+                {
+                    Console.WriteLine(number2);
+                }
+                else 
+                {
+                    Console.WriteLine(number3);
+                }
+            }
+        }
+    }
+}
