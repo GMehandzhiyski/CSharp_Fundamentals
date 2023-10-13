c525f56c7447a8d89aab48b1384aaeea1809d9e2
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-13 13:16:37 +0300

    05.AddAndSubtract 100% done

---
+++ 14 - Methods/14.Methods/05.AddAndSubtract/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _05.AddAndSubtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+
+            int sumOfNumber = SumOfNumber(number1, number2);
+            int subtractsOfNumbers = SubtractsOfNumbers(sumOfNumber, number3);
+
+            Console.WriteLine(subtractsOfNumbers);
+
+
+
+        }
+
+        private static int SubtractsOfNumbers(int sumOfNumber, int number3)
+        {
+            return sumOfNumber - number3;
+        }
+
+        private static int SumOfNumber(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
