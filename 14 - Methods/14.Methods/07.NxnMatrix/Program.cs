8ae06d14ae74fb96604fce88176993e5af22dd63
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-14 06:39:06 +0300

    07.NxnMatrix 100%done

---
+++ 14 - Methods/14.Methods/07.NxnMatrix/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _07.NxnMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int number = int.Parse(Console.ReadLine());
+
+            string firstRow = "";
+            for (int i = 0; i < number; i++)
+            {
+                firstRow += number + " ";
+            }
+            //Console.WriteLine(firstRow);    
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(firstRow);    
+            }
+        }
+    }
+}
