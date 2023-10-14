494d477b05e8dd8c8f4b69fe4722404ef130abf8
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-14 18:15:51 +0300

    08.FactorialDivision 20% done

---
+++ 14 - Methods/14.Methods/08.FactorialDivision/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _08.FactorialDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            double firstNumFactorial = CalculateFactorial(firstNumber);
+            double secondNumFactorial = CalculateFactorial(secondNumber);
+            Console.WriteLine($"{firstNumFactorial / secondNumber:f2}");
+
+        }
+
+        private static int CalculateFactorial(int firstNumber)
+        {
+            if (firstNumber == 0)
+                return 1;
+            else
+                return firstNumber * CalculateFactorial(firstNumber - 1);
+        }
+      
+    }
+}
---
+++ 14 - Methods/14.Methods/09.9. PalindromeIntegers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _09._9._PalindromeIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
