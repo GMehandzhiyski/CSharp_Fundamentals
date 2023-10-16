50f3f32f23b7bff0a0037702b542bcea354779dd
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-16 21:59:16 +0300

    03.HouseParty create

---
+++ 17 - List/18.ListExercise/03.HouseParty/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _03.HouseParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int commandsNumber = int.Parse(Console.ReadLine());
+            List <string> gests = new List<string>();
+
+            for (int i = 0; i < commandsNumber; i++)
+            { 
+                List <string> commands = Console.ReadLine()
+                      .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                      .ToList();
+                
+                if 
+                string middleString0 = commands[0];
+                string middleString1 = commands[1];
+
+
+
+
+
+
+
+                // Console.WriteLine(string.Join(" ",commands));    
+
+            }
+
+        }
+    }
+}
