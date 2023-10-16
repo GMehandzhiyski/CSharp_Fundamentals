b8a062ed5b8d1e23d2045c9473b4c2a9a3ae1f23
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-16 21:16:32 +0300

    01.Train 100% Done

--- 17 - List/18.ListExercise/01.Train/Program.cs
+++ 17 - List/18.ListExercise/01.Train/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> wagons = Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int maxCapacity = int.Parse(Console.ReadLine());
+
+            string command = null;
+
+            while ((command = Console.ReadLine()) != "end") 
+            {
+                string[] middleString = command.Split();
+                int passangers = 0;
+                int passangersAdd = 0;
+                if (middleString[0] == "Add" )
+                {
+                    passangers = int.Parse(middleString[1]);
+                    wagons.Add(passangers);
+                }
+                else
+                {
+
+                    passangersAdd = int.Parse(middleString[0]);
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        int fullSeats = wagons[i];
+                        int freeSeats = maxCapacity - fullSeats;
+                        if (freeSeats >= passangersAdd)
+                        {
+                            wagons[i] += passangersAdd;
+                            break;
+                        }
+                        
+                    }
+                    
+
+                }
+
+            }
+
+            Console.WriteLine(string.Join(" ", wagons));
         }
+
+       
     }
 }
