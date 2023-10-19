e21775ec448ce1eb9a86ccf7d751f08e62953ac3
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-19 06:30:27 +0300

    07.AppendArrays 100% Done

---
+++ 17 - List/18.ListExercise/07.AppendArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _07.AppendArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string [] stringArray = Console.ReadLine()
+                .Split('|',StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> finalArary = RemoveEmptySpace(stringArray);
+            Console.WriteLine(string .Join(" ", finalArary)); 
+
+        }
+
+        private static List<string> RemoveEmptySpace(string[] stringArray)
+        {
+            List<string> result = new List<string>();
+            for (int i = stringArray.Length - 1; i >= 0; i--)
+            {
+                string str = stringArray[i];
+                string[] arrays = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                result.AddRange(arrays);
+            }
+
+            return result;
+        }
+    }
+}
