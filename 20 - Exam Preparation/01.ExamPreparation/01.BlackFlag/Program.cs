9d460bc153a0d76f8927deeb18debdf335902497
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-21 23:27:08 +0300

    ExamPrepare211023

---
+++ 20 - Exam Preparation/01.ExamPreparation/01.BlackFlag/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _01.BlackFlag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal days = Math.Abs(decimal.Parse(Console.ReadLine()));
+            decimal plunderPerDay = Math.Abs(decimal.Parse(Console.ReadLine()));
+            decimal expectedPlunder = Math.Abs(decimal.Parse(Console.ReadLine()));
+           
+            decimal addPlunderEvery3days =(plunderPerDay * 1.5m);
+            decimal totalPlunder = 0;
+
+            for (int i = 1; i < days + 1; i++) 
+            {
+                if (i % 3 == 0)
+                {
+                    totalPlunder += addPlunderEvery3days;
+
+                }
+                else if (i % 5 == 0)
+                {
+                    totalPlunder += plunderPerDay;
+                    totalPlunder = totalPlunder - (totalPlunder * 0.30m);
+                }
+                else 
+                {
+                 totalPlunder += plunderPerDay;
+                }
+            
+            }
+            if (totalPlunder >= expectedPlunder)
+            {
+                Console.WriteLine($"Ahoy! {totalPlunder:f2} plunder gained.");
+            }
+            else 
+            {
+                decimal percentLeft = (totalPlunder / expectedPlunder) * 100;
+                Console.WriteLine($"Collected only {percentLeft:f2}% of the plunder.");
+            }
+
+            //Console.WriteLine(totalPlunder);
+
+            // each 3 day they add +50%(day + 50%)
+            // each 5 day they add -30%(total plunder - 30%)
+
+        }
+    }
+}
---
+++ 20 - Exam Preparation/01.ExamPreparation/02.TreasureHunt/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace _02.TreasureHunt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputList = Console.ReadLine()
+                .Split("|", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string arguments;
+            while ((arguments = Console.ReadLine()) != "Yohoho!")
+            {
+                string[] commands = arguments.Split();
+
+                if (commands[0] == "Loot")
+                {
+                    for (int i = 1; i < commands.Length; i++)
+                    {
+                        string treashureCommands = commands[i];
+                        int isFoundTreashure = inputList.IndexOf(treashureCommands);
+                        if (isFoundTreashure == -1)
+                        { 
+                            inputList.Insert(0, treashureCommands);
+                        }
+
+                    }
+
+                }
+                else if (commands[0] == "Drop")
+                {
+                    int index = int.Parse(commands[1]);
+                    if (0 >= index && index <= inputList.Count)
+                    { 
+                        string saveValue = inputList[index];
+                        inputList.RemoveAt(index);
+                        inputList.Add(saveValue);
+                    
+                    }
+
+                }
+                else if (commands[0] == "Steal")
+                {
+                    string printTreashure = string.Empty;
+                    int count = int.Parse(commands[1]); 
+                    for (int i = inputList.Count - 1 ; i >= count; i--)
+                    {
+                        string saveValue = inputList[i];
+
+                        printTreashure += saveValue + ", ";
+
+
+                    }
+                    
+                    Console.WriteLine(printTreashure);  
+
+                }
+
+            }
+            int countLetter = 0;
+            for (int i = 0; i < inputList.Count; i++) 
+            {
+
+                foreach (var item in inputList[i])
+                {
+                    countLetter++;
+                }
+
+            }
+
+            if (countLetter == 0)
+            {
+                Console.WriteLine("Failed treasure hunt.");
+            }
+            else
+            {
+                Console.WriteLine($"Average treasure gain: {countLetter / inputList.Count:f2} pirate credits.");
+            }
+            
+
+        }
+    }
+}
