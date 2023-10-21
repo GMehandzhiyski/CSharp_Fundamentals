c31a12f296acb1ea26893170a3683e781511a50f
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-21 17:13:20 +0300

    ExamPreparation211023

---
+++ 20 - Exam Preparation/01.ExamPreparation/02.ShoppingList/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace _02.ShoppingList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> productList = Console.ReadLine()
+                .Split("!",StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            string arguments;
+            while ((arguments = Console.ReadLine()) != "Go Shopping!")
+            {
+                string[] commands = arguments.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+                if (commands[0] == "Urgent")
+                {
+                    string item = commands[1];
+                    if (productList.IndexOf(item) == -1)
+                    { 
+                        productList.Insert(0, item);
+                    }
+
+                }
+                else if (commands[0] == "Unnecessary")
+                {
+                    string item = commands[1];
+                    int isFoundItem = productList.IndexOf(item); // -1 not found, 
+                    if (isFoundItem != -1)
+                    { 
+                        productList.RemoveAt(isFoundItem);
+                    }
+
+
+                }
+                else if (commands[0] == "Correct")
+                {
+                    string oldItem = commands[1];
+                    string newItem = commands[2];
+                    int isFoundItem = productList.IndexOf(oldItem);
+                    if (isFoundItem != -1)
+                    {
+                        productList[isFoundItem] = newItem;
+                    }
+
+                }
+                else if (commands[0] == "Rearrange")
+                {
+                    string item = commands[1];
+                    int isFoundItem = productList.IndexOf(item);
+                    if (isFoundItem != -1) 
+                    {
+                        string saveValue = productList[isFoundItem];
+                        productList.RemoveAt(isFoundItem);
+                        productList.Add(saveValue);
+                    
+                    }
+                }
+                else 
+                {
+                    continue;
+                }
+
+            
+            }
+            Console.WriteLine(string.Join(", ", productList));
+        }
+    }
+}
---
+++ 20 - Exam Preparation/01.ExamPreparation/03.Numbers/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _03.Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> allBiggerNumberList = new List<int>();
+            List<int> finalList = new List<int>();
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            decimal sumOfList = numbers.Sum();
+            decimal averageValue = (sumOfList / numbers.Count);
+            int numbersCount = 1;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] > averageValue)
+                {
+                    allBiggerNumberList.Add(numbers[i]);
+
+                }
+
+
+            }
+            if (allBiggerNumberList.Count == 0)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+
+            if (allBiggerNumberList.Count >= 5)
+            {
+                for (int j = 0; j <= (allBiggerNumberList.Count + 1); j++)
+                {
+                    //finalList[j] = allBiggerNumberList.Max();
+                    int maxValue = allBiggerNumberList.Max();
+                    finalList.Add(maxValue);
+                    allBiggerNumberList.Remove(maxValue);
+                    numbersCount++;
+                    if (numbersCount == 6)
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                finalList.AddRange(allBiggerNumberList);
+            }
+            Console.WriteLine(string.Join(" ", finalList.OrderByDescending(n => n).ToList()));
+            return;
+
+        }
+    }
+}
