e0b2838fb6b833b091c6abb9a5086dfba3f1348d
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-28 18:31:16 +0300

    102.Articles 100% done

---
+++ 22 - Object and Classes/22.ObjectsAndClasses/02.Articles/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _02.Articles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputList = Console.ReadLine()
+                .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            int numberCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberCommands; i++) 
+            {
+                string [] command = Console.ReadLine()
+                    .Split(": ",StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                if (command[0] == "Edit")
+                {
+                    inputList[1] = command[1];
+                }
+                else if (command[0] == "ChangeAuthor")
+                {
+                    inputList[2] = command[1];
+                }
+                else if (command[0] == "Rename")
+                {
+                    inputList[0] = command[1];
+                }
+            
+            }
+            Console.WriteLine($"{inputList[0]} - {inputList[1]}: {inputList[2]}");
+
+
+
+        }
+    }
+    class Article
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+    }
+}
---
+++ 22 - Object and Classes/22.ObjectsAndClasses/101. Advertisement Message/Program.cs
@@ -0,0 +1,99 @@
+﻿namespace _101._Advertisement_Message
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfRandomMess = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < numberOfRandomMess; i++)
+            {
+                string phrases = GetPhrases();
+                string events = GetEvents();
+                string authors  = GetAuthors();
+                string cities = GetCities();
+                Console.WriteLine($"{phrases} {events} {authors} - {cities}.");
+
+            }
+           
+        }
+
+        private static string GetCities()
+        {
+            
+            string phrasesNumberChois = string.Empty;
+
+            List<string> cities = new List<string>()
+                {"Burgas",
+                "Sofia",
+                "Plovdiv",
+                "Varna",
+                "Ruse"};
+
+            Random random = new Random();
+            int randomInt = random.Next(0, cities.Count);
+
+            phrasesNumberChois = cities[randomInt];
+            return phrasesNumberChois;
+        }
+
+        private static string GetAuthors()
+        {
+            string phrasesNumberChois = string.Empty;
+
+            List<string> authors = new List<string>()
+                {"Diana",
+                "Petya",
+                "Stella",
+                "Elena",
+                "Katya",
+                "Iva",
+                "Annie",
+                "Eva"};
+            Random random = new Random();
+            int randomInt = random.Next(0, authors.Count);
+
+            phrasesNumberChois = authors[randomInt];
+            return phrasesNumberChois;
+        }
+
+        private static string GetEvents()
+        {
+            string phrasesNumberChois = string.Empty;
+
+            List<string> events = new List<string>()
+            {"Now I feel good.",
+             "I have succeeded with this product.",
+             "Makes miracles. I am happy of the results!",
+             "I cannot believe but now I feel awesome.",
+             "Try it yourself, I am very satisfied.",
+             "I feel great!"};
+
+            Random random = new Random();
+            int randomInt = random.Next(0, events.Count);
+
+            phrasesNumberChois = events[randomInt];
+            return phrasesNumberChois;
+        }
+
+        private static string GetPhrases()
+        {
+            string phrasesNumberChois = string.Empty;
+
+            List<string> phrases = new List<string>()
+                { "Excellent product.",
+                "Such a great product.",
+                "I always use that product.",
+                "Best product of its category.",
+                "Exceptional product.",
+                "I can't live without this product." };
+
+            Random random = new Random();
+            int randomInt = random.Next(0, phrases.Count);
+
+            phrasesNumberChois = phrases[randomInt];
+            return phrasesNumberChois;
+        }
+    }
+}
