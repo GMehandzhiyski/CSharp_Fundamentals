f1e1252065e4946e424ad9c9d0eca4dac0680e17
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-28 14:38:11 +0300

    04.Students 100% Done

--- 22 - Object and Classes/22.ObjectsAndClasses/04.Students/Program.cs
+++ 22 - Object and Classes/22.ObjectsAndClasses/04.Students/Program.cs
@@ -1,10 +1,60 @@
 ﻿namespace _04.Students
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string arguments;
+            List<Students> finalListStudents = new List<Students>();
+            while ((arguments = Console.ReadLine()) != "end")
+            {
+                List<string> inputStidents = arguments
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                string firstName = inputStidents[0];
+                string lastName = inputStidents[1];
+                string age = inputStidents[2];
+                string homeTown = inputStidents[3];
+
+                Students students = new Students(firstName, lastName, age, homeTown);
+                finalListStudents.Add(students);
+
+            }
+
+            arguments = Console.ReadLine();
+            FoundTown(arguments, finalListStudents);
         }
+
+        private static void FoundTown(string arguments, List<Students> finalListStudents)
+        {
+            foreach (Students studentCycle in finalListStudents)
+            {
+                if (studentCycle.HomeTown == arguments)
+                {
+                    Console.WriteLine($"{studentCycle.FirstName} {studentCycle.LastName} is {studentCycle.Age} years old.");
+
+                }
+
+            }
+
+        }
+    }
+    class Students
+    {
+        public Students(string firstName, string lastName, string age, string homeTown)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            HomeTown = homeTown;
+
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string HomeTown { get; set; }
+
+
     }
 }
