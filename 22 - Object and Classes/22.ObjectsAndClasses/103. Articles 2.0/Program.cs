fb5b54ece7e9a24a23a11be7fe22206fcb3f9bcd
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-28 18:32:30 +0300

    103. Article 2.0 create

---
+++ 22 - Object and Classes/22.ObjectsAndClasses/103. Articles 2.0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _103._Articles_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
