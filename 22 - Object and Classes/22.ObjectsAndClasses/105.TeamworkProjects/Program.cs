9418a1df69bfaeb0b8422169872aaf74142b4320
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-29 10:45:00 +0200

    105.TeamworkProjects create

---
+++ 22 - Object and Classes/22.ObjectsAndClasses/105.TeamworkProjects/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _105.TeamworkProjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
