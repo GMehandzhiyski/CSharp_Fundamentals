7f6fe710f858a40268e97fea4997cc7550bb4b05
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-10-29 08:56:31 +0200

    103.Article2.0 100% done

--- 22 - Object and Classes/22.ObjectsAndClasses/103. Articles 2.0/Program.cs
+++ 22 - Object and Classes/22.ObjectsAndClasses/103. Articles 2.0/Program.cs
@@ -1,10 +1,46 @@
 ﻿namespace _103._Articles_2._0
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number = int.Parse(Console.ReadLine());
+            List<Articles> finalList = new List<Articles>();   
+
+            for (int i = 0; i < number; i++) 
+            {
+                List <string> inputList = Console.ReadLine()
+                    .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                string title = inputList[0];
+                string content = inputList[1];
+                string autor = inputList[2];    
+
+                Articles articles = new Articles(title, content, autor);
+                finalList.Add(articles);
+
+            }
+
+            foreach (Articles articles in finalList) 
+            {
+                Console.WriteLine($"{articles.Title} - {articles.Content}: {articles.Autor} ");
+            }
+
+        }
+        class Articles
+        {
+            public Articles(string title, string content, string autor)
+            {
+                Title = title;
+                Content = content;
+                Autor = autor;
+            }
+
+            public string Title { get; set; }
+            public string Content { get; set; }
+            public string Autor { get; set; }
+
         }
     }
 }
