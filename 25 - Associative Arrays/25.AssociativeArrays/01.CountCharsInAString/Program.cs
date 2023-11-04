7b2c7fa314f2b6b24f6d568f2398097bdb96d41b
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-04 09:42:40 +0200

    01.CountCharsInAString 100% done

---
+++ 25 - Associative Arrays/25.AssociativeArrays/01.CountCharsInAString/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _01.CountCharsInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+
+            Dictionary<char, int> letters = new Dictionary<char, int>();
+
+            foreach (char letter in inputString)
+            {
+                if (letter != ' ')
+                { 
+                    if (!letters.ContainsKey(letter))
+                    {
+                        letters.Add(letter, 1);
+                    }
+                    else
+                    {
+                        letters[letter]++;
+                    }
+                }
+            }
+
+            foreach (var currLetter in letters)
+            {
+                Console.WriteLine($"{currLetter.Key} -> {currLetter.Value}");
+            }
+
+        }
+    }
+}
