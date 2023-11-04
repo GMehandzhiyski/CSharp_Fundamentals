2d9966e71d6b1e52d6f38c96402b3241428d0043
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-04 07:19:44 +0200

    01.CountRealNumbers Create

---
+++ 25 - Associative Arrays/25.AssociativeArrays/01.CountRealNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _01.CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <int> inputNumbers = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            SortedDictionary<int, int> numbers = new SortedDictionary<int, int>();
+
+            foreach (int currNumber in inputNumbers) 
+            {
+                if (!numbers.ContainsKey(currNumber))
+                {   
+                    numbers.Add(currNumber, 1);
+                }
+                else
+                {
+                    numbers[currNumber]++;
+                }
+            
+            
+            }
+
+            PrintNumbers(numbers);
+        }
+
+        private static void PrintNumbers(SortedDictionary<int, int> numbers)
+        {   
+            foreach (KeyValuePair <int,int> currNumber in numbers) 
+            { 
+                Console.WriteLine($"{currNumber.Key} -> {currNumber.Value}");
+            }
+        }
+    }
+}
---
+++ 25 - Associative Arrays/25.AssociativeArrays/25.AssociativeArrays/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _25.AssociativeArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
