b5287c3a6398f68de7245d1db5031969fd4ded39
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-04 10:04:38 +0200

    02.AMinerTask 100% done

--- 25 - Associative Arrays/25.AssociativeArrays/02.AMinerTask/Program.cs
+++ 25 - Associative Arrays/25.AssociativeArrays/02.AMinerTask/Program.cs
@@ -1,10 +1,51 @@
 ﻿namespace _02.AMinerTask
 {
+/*
+Gold
+155
+Silver
+10
+Copper
+17
+stop
+
+
+
+     */
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary <string, int> minerResource = new Dictionary<string, int> ();
+
+            string argument;
+            while ((argument = Console.ReadLine()) != "stop") 
+            {
+                string resource = argument;
+                int quantity = int.Parse(Console.ReadLine());
+
+                    if (!minerResource.ContainsKey(resource))
+                    {
+                        minerResource.Add(resource, quantity);
+                    }
+                    else
+                    {
+                        minerResource[resource] += quantity;
+                    }
+                    
+                
+
+            }
+            PrintResources(minerResource);
+        }
+
+        private static void PrintResources(Dictionary<string, int> minerResource)
+        {
+            foreach (var currResource in minerResource)
+            {
+                Console.WriteLine($"{currResource.Key} -> {currResource.Value}");    
+                
+            }
         }
     }
 }
