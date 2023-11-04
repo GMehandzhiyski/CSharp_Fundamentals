2e4b0c1876b4cf8e1d3b80bfd103af141d7123c6
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-04 07:45:04 +0200

    02.OddOccurrence 100% done

--- 25 - Associative Arrays/25.AssociativeArrays/02.OddOccurrences/Program.cs
+++ 25 - Associative Arrays/25.AssociativeArrays/02.OddOccurrences/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> inputList = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            
+            Dictionary <string, int> occurrences = new Dictionary<string, int>(); 
+
+            foreach (string currOccurrences in inputList) 
+            {
+                string wordLower = currOccurrences.ToLower();
+
+                if (!occurrences.ContainsKey(wordLower))
+                {
+                    occurrences.Add(wordLower, 1);
+                }
+                else 
+                {
+                    occurrences[wordLower]++;
+                } 
+            }
+
+            foreach (KeyValuePair <string,int> currWord in occurrences)
+            {
+                if (currWord.Value % 2 == 1)
+                { 
+                    Console.Write($"{currWord.Key} ");
+                }
+            }
+
         }
     }
 }
