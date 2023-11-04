7cee67849bc32081ae23de7294287564c8880b8d
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-04 09:16:04 +0200

    04. Word Filter 100% done

---
+++ 25 - Associative Arrays/25.AssociativeArrays/04.WordFilter/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _04.WordFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputList = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            foreach (string currWord in inputList) 
+            {
+                if (currWord.Length % 2 == 0)
+                { 
+                    Console.WriteLine(currWord);
+                
+                }
+            
+            }
+        }
+    }
+}
