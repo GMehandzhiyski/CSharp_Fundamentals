b5197fa65ab7a13bceb71610b5b56a7c75bc82b9
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-05 06:52:47 +0200

    04.SoftUniParking 100% done

---
+++ 25 - Associative Arrays/25.AssociativeArrays/04.SoftUniParking/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace _04.SoftUniParking
+{
+/*
+5
+register John CS1234JS
+register George JAVA123S
+register Andy AB4142CD
+register Jesica VR1223EE
+unregister Andy
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberCommand = int.Parse(Console.ReadLine());
+
+            Dictionary <string, string> customersDateBase = new Dictionary<string, string>();
+
+            for (int i = 0; i < numberCommand; i++)
+            {
+                string[] argumnets = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string command = argumnets[0];
+                string customerName = argumnets[1];
+
+                if (command == "register")
+                {
+                    
+                    string licensePlate = argumnets[2];
+
+                    if (!customersDateBase.ContainsKey(customerName))
+                    {
+                        customersDateBase.Add(customerName, licensePlate);
+                        Console.WriteLine($"{customerName} registered {licensePlate} successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlate}");
+                    }
+                   
+                }
+                else if (command == "unregister")
+                {
+                    if (customersDateBase.ContainsKey(customerName))
+                    {
+                        bool deleteKey = customersDateBase.Remove(customerName);
+                        if (deleteKey) 
+                        {
+                            Console.WriteLine($"{customerName} unregistered successfully");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: user {customerName} not found");
+                    }
+                
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            foreach (KeyValuePair <string,string> currCustomer in customersDateBase)
+            {
+                Console.WriteLine($"{currCustomer.Key} => {currCustomer.Value}");
+            }
+        }
+    }
+}
