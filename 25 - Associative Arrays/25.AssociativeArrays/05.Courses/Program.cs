a5b57d60a38d3f90d8a3a70ff4bdbf074ca46d67
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-05 07:46:10 +0200

    05.Courses  100% done

--- 25 - Associative Arrays/25.AssociativeArrays/05.Courses/Program.cs
+++ 25 - Associative Arrays/25.AssociativeArrays/05.Courses/Program.cs
@@ -4,7 +4,64 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary <string,string> studentDataBase = new Dictionary <string,string> ();
+
+            string arguments = string.Empty;
+            while ((arguments = Console.ReadLine()) != "end") 
+            {
+                List <string> commands = arguments
+                    .Split(" : ",StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                string courseName = commands[0];
+                string studentName = commands[1];
+                if (!studentDataBase.ContainsKey(studentName))
+                {
+                   
+                    studentDataBase.Add(studentName, courseName);
+                }
+                else 
+                {
+                    continue;
+                }
+            
+            }
+
+
+            List <string> chekedCourse = new List <string> ();
+
+
+            foreach (KeyValuePair <string,string> currCourse in studentDataBase)
+            {
+                int counterCourse = 0;
+                if (!chekedCourse.Contains(currCourse.Value))
+                {
+                    foreach (KeyValuePair<string, string> foundCourse in studentDataBase)
+                    {
+                        if (currCourse.Value == foundCourse.Value)
+                        {
+                            counterCourse++;
+                        }
+
+                    }
+                    chekedCourse.Add(currCourse.Value);
+
+
+                    Console.WriteLine($"{currCourse.Value} : {counterCourse}");
+                    foreach (KeyValuePair<string, string> currStudentName in studentDataBase)
+                    {
+                        if (currCourse.Value == currStudentName.Value)
+                        {
+                            Console.WriteLine($"-- {currStudentName.Key}");
+                        }
+
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            }
         }
     }
 }
