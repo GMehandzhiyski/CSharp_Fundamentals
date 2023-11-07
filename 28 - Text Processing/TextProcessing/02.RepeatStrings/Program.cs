259577a0eb9f648a54f589f130963655639a1b02
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-07 15:06:01 +0200

    02.RepeatString 100%done

---
+++ 28 - Text Processing/TextProcessing/02.RepeatStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _02.RepeatStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string[] inputString = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string finalString = string.Empty;
+            foreach (string word in inputString)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    finalString += word;
+                }
+            }
+
+            Console.WriteLine(finalString);
+        }
+    }
+}
