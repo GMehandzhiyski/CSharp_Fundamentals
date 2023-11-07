870a8f4417de93e432b2b12e856ed78f176a2505
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-07 14:43:50 +0200

    01.RevereseString 100% done

---
+++ 28 - Text Processing/TextProcessing/01.ReverseStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01.ReverseStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString;
+            while ((inputString = Console.ReadLine()) != "end" ) 
+            {
+                string reversedString = string.Empty;
+                for (int i = inputString.Length -1 ; i >= 0 ; i--)
+                {
+                    reversedString += inputString[i];
+
+                }
+                Console.WriteLine($"{inputString} = {reversedString}");
+            }
+        }
+    }
+}
