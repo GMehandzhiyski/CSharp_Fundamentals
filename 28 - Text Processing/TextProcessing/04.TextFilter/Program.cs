c03b94d724e8d9b8af105e4388a27fff997bc359
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-08 10:29:49 +0200

    04.TextFilter 100%done

---
+++ 28 - Text Processing/TextProcessing/04.TextFilter/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _04.TextFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputWords = Console.ReadLine()
+                 .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                 .ToList();
+
+            string inputText = Console.ReadLine();
+
+            for (int i = 0; i < inputWords.Count; i++)
+            {
+
+                string replaceString = string.Empty;
+                for (int j = 0; j < inputWords[i].Length; j++)
+                {
+                    replaceString = replaceString + "*";
+                }
+
+                inputText = inputText.Replace(inputWords[i], replaceString);
+
+            }
+           
+            Console.WriteLine(inputText);
+           
+        }
+    }
+}
