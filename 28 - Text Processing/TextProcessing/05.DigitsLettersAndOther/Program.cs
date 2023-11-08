71736674e8e16134e42572160168ba12c2722f2a
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-08 10:37:00 +0200

    05.DigitsLettersAndOther 100% done

---
+++ 28 - Text Processing/TextProcessing/05.DigitsLettersAndOther/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace _05.DigitsLettersAndOther
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = (Console.ReadLine());
+            StringBuilder stringBuilderNumber = new StringBuilder();
+            StringBuilder stringBuilderLetter = new StringBuilder();
+            StringBuilder stringBuilderSpecialSymbol = new StringBuilder();
+
+
+            foreach (var symbol in inputString)
+            {
+                if (char.IsDigit(symbol))
+                {
+                    stringBuilderNumber.Append(symbol);
+                }
+                else if (char.IsLetter(symbol))
+                {
+                    stringBuilderLetter.Append(symbol);    
+                }
+                else
+                {
+                    stringBuilderSpecialSymbol.Append(symbol);  
+                }
+            }
+
+            PrintAllStrings(stringBuilderNumber, stringBuilderLetter, stringBuilderSpecialSymbol);
+        }
+
+        private static void PrintAllStrings(StringBuilder stringBuilderNumber, StringBuilder stringBuilderLetter, StringBuilder stringBuilderSpecialSymbol)
+        {
+            Console.WriteLine(stringBuilderNumber.ToString());
+            Console.WriteLine(stringBuilderLetter.ToString());
+            Console.WriteLine(stringBuilderSpecialSymbol.ToString());
+        }
+    }
+}
