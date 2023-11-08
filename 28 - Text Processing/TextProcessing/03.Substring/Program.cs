69c5e0eaa9e30c4a73c4946a4ae99792a96b4302
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-08 10:28:58 +0200

    03.Substring 100%done

---
+++ 28 - Text Processing/TextProcessing/03.Substring/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _03.Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstString = Console.ReadLine();
+            string secondString = Console.ReadLine();
+
+            for (int i = 0; i < secondString.Length; i++)
+            {
+                int isIndexValid = secondString.LastIndexOf(firstString);
+                secondString = secondString.Remove(isIndexValid, firstString.Length);
+            }
+
+            Console.WriteLine(secondString);
+        }
+    }
+}
