9eeb86f853f8524c4f72cf972d669fc7c7fbd578
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-08 11:19:47 +0200

    01.ValidUsernames 100%done

---
+++ 28 - Text Processing/TextProcessing/01.ValidUsernames/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _01.ValidUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputString = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            foreach (var userName in inputString)
+            {
+
+                bool isUserNameRightLenght = CheckUserNameLenght(userName);
+                bool isUserNameCorrect = CheckUserName(userName);
+                if (isUserNameRightLenght
+                    && isUserNameCorrect)
+                {
+                    PrintCorrectUserName(userName);
+                }
+            }
+        }
+
+        private static void PrintCorrectUserName(string userName)
+        {
+            Console.WriteLine(userName);
+        }
+
+        private static bool CheckUserNameLenght(string userName)
+        {
+            return (userName.Length >= 3) && (userName.Length <= 16);
+        }
+
+        private static bool CheckUserName(string userName)
+        {
+            return  userName.All(u => char.IsLetterOrDigit(u) || u == '-' || u == '_');
+        }
+    }
+}
