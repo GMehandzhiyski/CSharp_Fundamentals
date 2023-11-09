d330e443fb6a21c9cbf6c8df05806582abc187e4
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-09 13:46:03 +0200

    06.ReplaceRepeatingChars 100% done

---
+++ 28 - Text Processing/TextProcessing/06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Common;
+using System.Text;
+
+namespace _06.ReplaceRepeatingChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+            char lastLetter = '\0';
+         
+            StringBuilder sb = new StringBuilder(); 
+
+            foreach (var currLetter in inputString)
+            {
+                if (currLetter != lastLetter)
+                {
+                    sb.Append(currLetter);
+                    lastLetter = currLetter;
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
