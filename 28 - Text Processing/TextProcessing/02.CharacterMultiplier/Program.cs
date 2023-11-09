b7e5fa6e19bf58e394568b987b169a449a440285
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-09 05:48:21 +0200

    02.CharacterMultiplier 100%done

---
+++ 28 - Text Processing/TextProcessing/02.CharacterMultiplier/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _02.CharacterMultiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputString = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            if (inputString[0].Length >= inputString[1].Length)
+            {
+                foreach (var argumnets in inputString[0])
+                {
+                    Console.WriteLine(argumnets);
+                }
+            }
+            else
+            {
+                foreach (var argumnets in inputString[1])
+                {
+                    Console.WriteLine(argumnets);
+                }
+            }
+        }
+    }
+}
