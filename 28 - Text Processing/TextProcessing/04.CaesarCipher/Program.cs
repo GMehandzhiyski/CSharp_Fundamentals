9c24b9141289d9b8885e5575d3cb906849183cb8
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-09 06:08:44 +0200

    04.CaesarCipher 100% done

---
+++ 28 - Text Processing/TextProcessing/04.CaesarCipher/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace _04.CaesarCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string inputString = Console.ReadLine();
+
+            PrintEncryptString(inputString);
+           
+        }
+
+        private static void PrintEncryptString(string inputString)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var letter in inputString)
+            {
+                sb.Append((char)(letter + 3));
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
