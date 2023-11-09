a6389156e5ef7d65ec830211c4e31439c2dc5f95
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-09 05:48:38 +0200

    03.ExtractFile 100%done

---
+++ 28 - Text Processing/TextProcessing/03.ExtractFile/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _03.ExtractFile
+{
+    /*C:\Internal\training-internal29\Template38.pptx*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string inputString = Console.ReadLine();
+           
+           int fileExtensionPosition = inputString.LastIndexOf('.');//,inputString.Length-1
+            string fileExtension = inputString.Substring(fileExtensionPosition + 1,((inputString.Length - 1) - fileExtensionPosition));
+
+            int fileNamePosition = inputString.LastIndexOf('\\');//, inputString.Length - 1
+            int stringLength = inputString.Length;
+            string fileName = inputString.Substring(fileNamePosition + 1,fileExtensionPosition - fileNamePosition - 1);
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
+        }
+    }
+}
