cd02f699e486ed1ad61b7a950baa4d3ae748bfa6
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-14 22:14:16 +0200

    01.MatchFullName 100%done

---
+++ 31 - Regular Expressions/31-RegularExpressionns/01.MatchFullName/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _01.MatchFullName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+
+            string pattern = @"\b(?<firstname>[A-Z][a-z]+) (?<secondname>[A-Z][a-z]+)\b";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = Regex.Matches(inputString, pattern);
+
+            string finalStringName = string.Empty;
+            foreach (Match match in matches)
+            {
+                finalStringName += match.Value + " ";
+            }
+
+            Console.WriteLine(finalStringName);
+
+        }
+    }
+}
