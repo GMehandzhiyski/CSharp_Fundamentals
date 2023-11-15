ddfa70cc3b9d1d3464a326e92a0f57664aa7e907
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-15 19:41:49 +0200

    02.MatchPhoneNumber 100%done

---
+++ 31 - Regular Expressions/31-RegularExpressionns/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          string phoneNumber = Console.ReadLine();
+
+             //string pattern = @"(?<phoneNumber>\+\d{3}\ \d{1} \d{3} \d{4})|(?<phoneNumber>\+\d{3}\-\d{1}-\d{3}-\d{4}\b)";
+            string pattern = @"(?<phoneNumber>\+([359]+)([- ])2(\2)(\d{3})(\2)(\d{4})\b)";
+
+            MatchCollection matches = Regex.Matches(phoneNumber, pattern);
+            
+            string finalString = string.Empty;
+            foreach (Match match in matches)
+            {
+                finalString += match.Groups["phoneNumber"].Value + ", ";
+            }
+            finalString = finalString.TrimEnd(' ');
+            finalString = finalString.TrimEnd(',');
+            Console.WriteLine(finalString);
+        }
+    }
+}
