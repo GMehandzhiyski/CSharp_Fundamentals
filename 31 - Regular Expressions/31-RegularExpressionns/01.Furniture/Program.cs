62593ff347c495461c3ef786acd9e5ae84b8f597
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-15 21:07:02 +0200

    _01.Furniture  100%done

---
+++ 31 - Regular Expressions/31-RegularExpressionns/01.Furniture/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _01.Furniture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listProsucts = new List<string>();
+
+            string pattern = @">>(?<name>[A-Za-z]+)<<(?<price>\d+.\d+)!(?<quantity>\d+)";
+            decimal totalManey = 0;
+
+            string arguments;
+            while ((arguments = Console.ReadLine()) != "Purchase")
+            {
+                MatchCollection matches = Regex.Matches(arguments, pattern);
+
+                
+             
+                foreach (Match match in matches)
+                {
+                    //Console.WriteLine(match.Groups["name"].Value);
+                    decimal price = decimal.Parse(match.Groups["price"].Value);
+                    decimal quantity = decimal.Parse(match.Groups["quantity"].Value);
+                    listProsucts.Add(match.Groups["name"].Value);
+                    totalManey += price * quantity;
+
+                }
+            }
+            Console.WriteLine("Bought furniture:");
+            foreach (var name in listProsucts)
+            {
+                Console.WriteLine(name); 
+            }
+            Console.WriteLine($"Total money spend: {totalManey}");
+
+        }
+    }
+}
