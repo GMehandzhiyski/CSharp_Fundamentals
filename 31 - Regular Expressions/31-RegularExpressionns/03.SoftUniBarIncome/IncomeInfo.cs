2c4cafcd4a106f41c61dca1370e935a113054031
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-17 15:36:31 +0200

    03.SoftUniBarIncome 100% done

---
+++ 31 - Regular Expressions/31-RegularExpressionns/03.SoftUniBarIncome/IncomeInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace _03.SoftUniBarIncome
+{
+    internal class IncomeInfo
+    {
+        private string name;
+        private string product;
+        private int quantity;
+        private decimal price;
+
+        public IncomeInfo(string name, string product, int quantity, decimal price)
+        {
+            this.name = name;
+            this.product = product;
+            this.quantity = quantity;
+            this.price = price;
+        }
+    }
+}
---
+++ 31 - Regular Expressions/31-RegularExpressionns/03.SoftUniBarIncome/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _03.SoftUniBarIncome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string pattern = @"\%(?<name>[A-Z][a-z]+)\%[^|'$%.]*\<(?<product>[A-Za-z]+)\>[^|'$%.]*\|(?<quantity>\d+)\|[^|'$%.]*?(?<price>\d+|\d+\.\d+)\$";
+            string pattern = @"^%(?<customer>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.]*?(?<price>[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)\$";
+            List<IncomeInfo> listIncomeInfo = new List<IncomeInfo>();
+
+            string argumets = string.Empty;
+            while ((argumets = Console.ReadLine()) != "end of shift") 
+            {
+                MatchCollection matches = Regex.Matches(argumets, pattern);
+
+                foreach (Match match in matches)
+                {
+                    string name = match.Groups["customer"].Value;
+                    string product = match.Groups["product"].Value;
+                    int quantity = int.Parse(match.Groups["count"].Value);
+                    decimal price = decimal.Parse(match.Groups["price"].Value);
+
+                    IncomeInfo incomeInfo = new IncomeInfo(name, product, quantity, price);
+                    listIncomeInfo.Add(incomeInfo); 
+                }
+            }
+
+            PrintFinalIncomeInfo(listIncomeInfo);
+        }
+
+        private static void PrintFinalIncomeInfo(List<IncomeInfo> listIncomeInfo)
+        {
+            decimal totalPrice = 0;
+            decimal totalIncome = 0;
+
+            foreach (var item in listIncomeInfo)
+            {  
+                totalPrice = item.Price * item.Quantity;
+                Console.WriteLine($"{item.Name:f2}: {item.Product:f2} - {totalPrice:f2}");
+                totalIncome += totalPrice;
+            }
+
+            Console.WriteLine($"Total income: {totalIncome:f2}");
+        }
+
+        public class IncomeInfo
+        {
+            public IncomeInfo(string name, string product, int quantity, decimal price)
+            {
+                Name = name;
+                Product = product;
+                Quantity = quantity;    
+                Price = price;  
+                
+            }
+            public string Name { get; set; }
+            public string Product { get; set; }
+
+            public int Quantity { get; set; }
+
+            public decimal Price  { get; set; }
+
+        }
+       
+    }
+
+}
