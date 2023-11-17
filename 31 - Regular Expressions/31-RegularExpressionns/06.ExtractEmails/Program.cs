0a3fb04251ad21284e642283abe503fc757889fe
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-17 06:33:17 +0200

    06.ExtractEmails 100%done

---
+++ 31 - Regular Expressions/31-RegularExpressionns/06.ExtractEmails/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _06.ExtractEmails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+            //string regexPattern = @"(?<email>[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z][\w\.-]+\.[a-zA-Z]{2,})";
+            string regexPattern = @"(?<email>(?<![^\s])([a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z]{2,}))";
+
+            MatchCollection matches = Regex.Matches(inputString, regexPattern);
+
+            foreach (Match match in matches) 
+            {
+                Console.WriteLine(match.Groups["email"].Value);
+            }
+        }
+    }
+}
