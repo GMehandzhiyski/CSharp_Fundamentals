50b1721cde5c5b74a32503e8091324ddda89057f
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-17 15:36:50 +0200

    03._Match_Dates 100% done

---
+++ 31 - Regular Expressions/31-RegularExpressionns/03. Match Dates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+
+            string pattern = @"((?<day>\d{2})([\/\-.])(?<month>Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\2(?<year>\d{4}))";
+
+            MatchCollection matches = Regex.Matches(inputString, pattern);
+
+            foreach (Match match in matches) 
+            {
+                var day = match.Groups["day"].Value;
+                var month = match.Groups["month"].Value;
+                var year = match.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
