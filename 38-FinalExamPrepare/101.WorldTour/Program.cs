18bd0d6d9f78027e98d72b2625a8ab9567d808a1
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-18 19:53:10 +0200

    101.WordTour create

---
+++ 38-FinalExamPrepare/101.WorldTour/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _101.WorldTour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
