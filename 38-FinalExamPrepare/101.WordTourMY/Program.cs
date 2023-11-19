2c81d812885f017839a0f3adf22ca9a6f348db3a
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-19 07:06:27 +0200

    101.WorldTour 100% done

---
+++ 38-FinalExamPrepare/101.WordTourMY/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+namespace _101.WorldTour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(inputString);
+
+            string arguments = string.Empty;
+            while ((arguments = Console.ReadLine()) != "Travel")
+            {
+                string[] command = arguments
+                    .Split(":", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                if (command[0] == "Add Stop")
+                {
+                    int index = int.Parse(command[1]);
+                    bool isIndexIsValid = CheckForValidIndex(index, sb);
+                    if (isIndexIsValid)
+                    {
+                        string destination = command[2];
+                        sb.Insert(index, destination);
+                    }
+                }
+
+                else if (command[0] == "Remove Stop")
+                {
+                    int startIndex = int.Parse(command[1]);
+                    int endIndex = int.Parse(command[2]);
+
+                    bool isIndexsIsValid = CheckForValidIndexs(startIndex, endIndex, sb);
+                    if (isIndexsIsValid)
+                    {
+                        sb.Remove(startIndex, (endIndex - startIndex) + 1);
+                    }
+
+                }
+                else if (command[0] == "Switch")
+                {
+                    string oldIndex = command[1];
+                    string newIndex = command[2];
+                    bool isOldIndexIsAvalivable = CheckOldIndex(oldIndex, sb);
+                    while (isOldIndexIsAvalivable)
+                    {
+                        sb.Replace(oldIndex, newIndex);
+                        isOldIndexIsAvalivable = CheckOldIndex(oldIndex, sb);
+                    }
+
+                }
+                else
+                {
+                    continue;
+                }
+                Console.WriteLine(sb.ToString());
+            }
+
+            Console.WriteLine($"Ready for world tour! Planned stops: {sb.ToString()}");
+        }
+
+        private static bool CheckOldIndex(string oldIndex, StringBuilder sb)
+        {
+            bool containsResult;
+            return containsResult = sb.ToString().Contains(oldIndex);
+        }
+
+        private static bool CheckForValidIndexs(int startIndex, int endIndex, StringBuilder sb)
+        {
+            return (startIndex >= 0 && startIndex <= sb.Length)
+                    && (endIndex <= sb.Length);
+        }
+
+        private static bool CheckForValidIndex(int index, StringBuilder sb)
+        {
+            return index >= 0 && index <= sb.Length;
+        }
+    }
+}
--- 38-FinalExamPrepare/101.WorldTour/Program.cs
+++ 38-FinalExamPrepare/101.WorldTour/Program.cs
@@ -1,10 +1,64 @@
-﻿namespace _101.WorldTour
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+namespace _101.WorldTour
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string inputString = Console.ReadLine();
+
+            string arguments = string.Empty;
+            while ((arguments = Console.ReadLine()) != "Travel")
+            {
+                string[] commands = arguments
+                    .Split(":", StringSplitOptions.RemoveEmptyEntries);
+                    
+                string command = commands[0];
+                if (command == "Add Stop")
+                {
+                    int index = int.Parse(commands[1]);
+                    if (index >= 0 
+                        && index <= inputString.Length)
+                    {
+                        string destination = commands[2];
+                        inputString = inputString.Insert(index, destination);
+                    }
+                }
+
+                else if (command == "Remove Stop")
+                {
+                    int startIndex = int.Parse(commands[1]);
+                    int endIndex = int.Parse(commands[2]);
+
+                    if (startIndex >= 0 
+                        && endIndex <= inputString.Length)
+                    {
+                        inputString = inputString.Remove(startIndex, (endIndex - startIndex) + 1);
+                    }
+
+                }
+                else if (command == "Switch")
+                {
+                    string oldIndex = commands[1];
+                    string newIndex = commands[2];
+                   
+                    while (inputString.Contains(oldIndex))
+                    {
+                        inputString = inputString.Replace(oldIndex, newIndex);
+                    }
+
+                }
+              
+                Console.WriteLine(inputString);
+            }
+
+            Console.WriteLine($"Ready for world tour! Planned stops: {inputString}");
         }
+
+   
+       
     }
 }
