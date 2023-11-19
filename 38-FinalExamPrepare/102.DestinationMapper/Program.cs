67fada35e0f0b12048e121b3c21c8de21d37537a
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-19 07:06:43 +0200

    102.DestinationMapper Create

---
+++ 38-FinalExamPrepare/102.DestinationMapper/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _102.DestinationMapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
