93a342460b89323642ae7642d61378f243847585
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-19 08:25:22 +0200

    102.DestinationMapper 100% done

--- 38-FinalExamPrepare/102.DestinationMapper/Program.cs
+++ 38-FinalExamPrepare/102.DestinationMapper/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace _102.DestinationMapper
+﻿using System.Text.RegularExpressions;
+
+namespace _102.DestinationMapper
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string regexPattern = @"([=\/])((?<destination>[A-Z][A-Za-z]{2,}))\1";
+
+            string inputString = Console.ReadLine();    
+
+            MatchCollection matches = Regex.Matches(inputString, regexPattern);
+
+            int stringLenght = 0;
+            string destination = string.Empty;
+
+            foreach (Match match in matches)
+            {
+                stringLenght += match.Groups["destination"].Value.Length;
+                destination += match.Groups["destination"].Value + ", ";
+            }
+
+            PrintResultOfExample(destination, stringLenght);
+        }
+
+        private static void PrintResultOfExample(string destination, int stringLenght)
+        {
+           
+            Console.WriteLine($"Destinations: {destination.TrimEnd(',', ' ')}");
+            Console.WriteLine($"Travel Points: {stringLenght}");
         }
     }
 }
