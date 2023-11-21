7a42a59be680bdc63ca291c22eaf1706bfb70f68
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-21 21:37:04 +0200

    303.NeedForSpeed3 create

---
+++ 38-FinalExamPrepare/303.NeedForSpeed3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _303.NeedForSpeed3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
