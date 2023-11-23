eb27ffb0a928a5e5d97500bb25df05d438abb13c
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-23 15:12:43 +0200

    403._HeroesОfCodeАndLogicVII 100% done

---
+++ 38-FinalExamPrepare/403. HeroesОfCodeАndLogicVII/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _403._HeroesОfCodeАndLogicVII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
