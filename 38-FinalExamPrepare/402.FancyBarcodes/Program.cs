b58dd7b7e778e7d08c2cab665e378900be68a88a
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-23 15:13:09 +0200

    402.FancyBarcodes 100% done

--- 38-FinalExamPrepare/402.FancyBarcodes/Program.cs
+++ 38-FinalExamPrepare/402.FancyBarcodes/Program.cs
@@ -1,10 +1,76 @@
-﻿namespace _402.FancyBarcodes
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
+
+namespace _402.FancyBarcodes
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number = int.Parse(Console.ReadLine());
+
+            string regexPattern = @"[@]{1}[#]+(?<string>[A-Za-z0-9]{5,}[A-Za-z]+[0-9]*[A-Z0-9]*)[@]{1}[#]+";
+
+            for (int i = 0; i < number; i++)
+            {
+                string inputString = Console.ReadLine();
+
+                MatchCollection Matches = Regex.Matches(inputString, regexPattern);
+
+                if (Matches.Count != 0)
+                {
+                    string productGroup =  FoundProductGroup(inputString, regexPattern, Matches);
+
+                    PrintFinalResul(productGroup);
+                    
+                }
+                else 
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+
+               
+
+            }
+
         }
+
+        private static void PrintFinalResul(string productGroup)
+        {
+            string productGroupFinal = string.Empty;
+
+            if (productGroup != "")
+            {
+                productGroupFinal = productGroup;
+            }
+            else 
+            {
+                productGroupFinal = "00";
+            }
+            Console.WriteLine($"Product group: {productGroupFinal}");
+        }
+
+        public static string FoundProductGroup(string inputString, string regexPattern, MatchCollection Matches)
+        {
+            string digits = string.Empty;
+           
+
+            foreach (Match item in Matches)
+            {
+                string barcode = item.Groups["string"].Value;
+
+                foreach (var currChar in barcode)
+                {
+                    bool isDigit = char.IsDigit(currChar);
+                    if (isDigit) 
+                    {
+                        digits += currChar;
+                    }
+                }
+            }
+            return digits;
+        }
+
     }
 }
