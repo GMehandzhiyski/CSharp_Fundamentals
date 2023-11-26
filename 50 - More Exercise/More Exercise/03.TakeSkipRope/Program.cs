f6d559195828aa82e4910ecf2defb1ebc574ea74
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-26 08:12:19 +0200

    03.TakeSkipRope create

---
+++ 50 - More Exercise/More Exercise/03.TakeSkipRope/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _03.TakeSkipRope
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
