c0e9c79e56461be0f892349a966f78be946e9a86
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-29 07:20:47 +0200

    04.MixedupLists 100% done

---
+++ 50 - More Exercise/More Exercise/05.DrumSet/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _05.DrumSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal saveMoney = decimal.Parse(Console.ReadLine());
+
+            List<int> drumQualityFirst = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            List<int> drumQualityCurr = new List<int>();
+            drumQualityCurr.AddRange(drumQualityFirst);
+
+            string arguments = string.Empty;
+            while ((arguments = Console.ReadLine()) != "Hit it again, Gabsy!")
+            {
+                int decreasePower = int.Parse(arguments);
+                for (int i = 0; i < drumQualityCurr.Count; i++)
+                {
+                    drumQualityCurr[i] -= decreasePower;
+                    if (drumQualityCurr[i] <= 0)
+                    {
+                        if (saveMoney >= (drumQualityFirst[i] * 3))
+                        {
+                            saveMoney -= drumQualityFirst[i] * 3;
+                            drumQualityCurr[i] = drumQualityFirst[i];
+                        }
+                        else
+                        {
+                            drumQualityCurr.RemoveAt(i);
+                            drumQualityFirst.RemoveAt(i);
+                            i--;
+                        }
+                    }
+                 }
+                  
+            }
+
+            Console.WriteLine(string.Join(" ",drumQualityCurr));
+            Console.WriteLine($"Gabsy has {saveMoney:f2}");
+        }
+    }
+}
