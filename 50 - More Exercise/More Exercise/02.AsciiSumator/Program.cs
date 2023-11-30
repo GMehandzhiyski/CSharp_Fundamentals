7043f3e6fd14f1d478fd49d72fa641a3e5e7bde7
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-11-30 13:54:49 +0200

    02.AsciiSumator 100% done

---
+++ 50 - More Exercise/More Exercise/02.AsciiSumator/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _02.AsciiSumator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char startChar = char.Parse(Console.ReadLine());
+            char endChar = char.Parse(Console.ReadLine());
+            string inputString = Console.ReadLine();
+            string middleString = string.Empty;
+            foreach (var currChar in inputString)
+            {
+                if (currChar > startChar
+                    && currChar < endChar)
+                {
+                    middleString += currChar;
+                }
+            }
+            int sumOfChar = 0;
+            foreach (var currChar in middleString)
+            {
+                int charToInt = currChar;
+                sumOfChar += charToInt;
+            }
+
+            Console.WriteLine(sumOfChar);
+        }
+    }
+}
