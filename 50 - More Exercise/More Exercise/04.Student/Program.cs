33f7be3e1fecdbef3bf296924691a99adfcccef8
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2023-12-01 06:10:56 +0200

    204.Student 100% done

---
+++ 50 - More Exercise/More Exercise/04.Student/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _04.Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStudent = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();   
+
+            for (int i = 0; i < numberOfStudent; i++)
+            {
+                string[] inputStudent = Console.ReadLine()
+                    .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string firstName = inputStudent[0];
+                string lastName = inputStudent[1];  
+                double grade = double.Parse(inputStudent[2]);
+
+                Student student = new Student(firstName, lastName, grade); 
+                students.Add(student);
+            }
+
+            foreach (Student currStudent in students.OrderByDescending(s => s.Grade))
+            {
+                Console.WriteLine($"{currStudent.FirstName} {currStudent.LastName}: {currStudent.Grade}");
+            }
+        }
+    }
+    public class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+    }
+}
---
+++ 50 - More Exercise/More Exercise/202.Articles/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq;
+
+namespace _202.Articles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputString = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            string title = inputString[0];
+            string content = inputString[1];
+            string author = inputString[2];
+
+            List<Article> articleList = new List<Article>();
+
+            Article article = new Article(title, content, author);
+            articleList.Add(article);
+
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                string[] commands = Console.ReadLine()
+                    .Split(":", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string command = commands[0];
+                string newText = commands[1];
+
+                if (command == "Edit")
+                {
+                    EditContent(articleList, newText);
+                }
+                else if (command == "ChangeAuthor")
+                {
+                    ChangeAuthor(articleList,newText);
+                }
+                else if (command == "Rename")
+                {
+                    Rename(articleList, newText);
+                }
+            }
+
+            Console.WriteLine($"{articleList[0].Title} -{articleList[0].Content}:{articleList[0].Author}");
+        }
+
+        private static void Rename(List<Article> articleList, string newText)
+        {
+            articleList[0].Title = newText;
+        }
+
+        private static void ChangeAuthor(List<Article> articleList, string newText)
+        {
+            articleList[0].Author = newText;
+        }
+
+        public static void EditContent(List<Article> articleList, string newText)
+        {
+            articleList[0].Content = newText;
+        }
+    }
+    public class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        
+    }
+}
